8ef827865a43c9274d4cb4ee2b715f7142483be9
mongalong247 <ian.minimad@gmail.com>
2025-06-12 19:43:36 +1200

    Update ui.py
    adding ability to select individual files not just folders.

--- ui.py
+++ ui.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtCore import QThread
 from import_worker import ImportWorker
 from utils import truncate_path
+from metadata_panel import MetadataPanel
 
 class ImageImporter(QWidget):
     def __init__(self):
@@ -14,6 +15,7 @@ class ImageImporter(QWidget):
         self.layout = QHBoxLayout(self)
 
         self.source_folder = ""
+        self.selected_files = []
         self.dest_folder = ""
         self.backup_folder = ""
 
@@ -76,63 +78,32 @@ class ImageImporter(QWidget):
         self.importLayout.addWidget(self.progress)
 
     def build_metadata_panel(self):
-        self.metadata_panel = QGroupBox("Metadata Entry")
-        self.metadata_layout = QGridLayout()
-        self.metadata_panel.setLayout(self.metadata_layout)
-
-        self.make_input = QLineEdit()
-        self.model_input = QLineEdit()
-
-        self.metadata_layout.addWidget(QLabel("Lens Make:"), 0, 0)
-        self.metadata_layout.addWidget(self.make_input, 0, 1)
-        self.metadata_layout.addWidget(QLabel("Lens Model:"), 1, 0)
-        self.metadata_layout.addWidget(self.model_input, 1, 1)
-
-        self.focal_checkbox = QCheckBox("Add Focal Length")
-        self.focal_input = QLineEdit()
-        self.focal_input.setPlaceholderText("e.g., 85mm")
-        self.focal_input.setEnabled(False)
-
-        self.aperture_checkbox = QCheckBox("Add Aperture")
-        self.aperture_input = QLineEdit()
-        self.aperture_input.setPlaceholderText("e.g., 2.8")
-        self.aperture_input.setEnabled(False)
-
-        self.serial_checkbox = QCheckBox("Add Lens Serial")
-        self.serial_input = QLineEdit()
-        self.serial_input.setPlaceholderText("Optional")
-        self.serial_input.setEnabled(False)
-
-        self.notes_checkbox = QCheckBox("Add Notes")
-        self.notes_input = QTextEdit()
-        self.notes_input.setEnabled(False)
-
-        self.focal_checkbox.stateChanged.connect(lambda: self.focal_input.setEnabled(self.focal_checkbox.isChecked()))
-        self.aperture_checkbox.stateChanged.connect(lambda: self.aperture_input.setEnabled(self.aperture_checkbox.isChecked()))
-        self.serial_checkbox.stateChanged.connect(lambda: self.serial_input.setEnabled(self.serial_checkbox.isChecked()))
-        self.notes_checkbox.stateChanged.connect(lambda: self.notes_input.setEnabled(self.notes_checkbox.isChecked()))
-
-        self.metadata_layout.addWidget(self.focal_checkbox, 2, 0)
-        self.metadata_layout.addWidget(self.focal_input, 2, 1)
-        self.metadata_layout.addWidget(self.aperture_checkbox, 3, 0)
-        self.metadata_layout.addWidget(self.aperture_input, 3, 1)
-        self.metadata_layout.addWidget(self.serial_checkbox, 4, 0)
-        self.metadata_layout.addWidget(self.serial_input, 4, 1)
-        self.metadata_layout.addWidget(self.notes_checkbox, 5, 0)
-        self.metadata_layout.addWidget(self.notes_input, 5, 1, 2, 1)
-
+        self.metadata_panel = MetadataPanel()
         self.metadata_panel.setVisible(False)
 
     def toggle_metadata_panel(self, text):
         self.metadata_panel.setVisible(text == "Yes")
 
     def select_source(self):
-        folder = QFileDialog.getExistingDirectory(self, "Select Source Folder")
-        if folder:
-            self.source_folder = folder
-            display_path = truncate_path(folder)
-            self.source_path_label.setText(display_path)
-            self.source_path_label.setToolTip(folder)
+        choice, _ = QFileDialog.getOpenFileNames(
+            self,
+            "Select Files or Cancel to Choose a Folder",
+            "",
+            "Images (*.jpg *.jpeg *.png *.cr2 *.nef *.arw *.dng)"
+        )
+        if choice:
+            self.selected_files = choice
+            self.source_folder = ""  # Clear folder if files selected
+            self.source_path_label.setText(f"{len(choice)} file(s) selected")
+            self.source_path_label.setToolTip("\n".join(choice))
+        else:
+            folder = QFileDialog.getExistingDirectory(self, "Select Source Folder")
+            if folder:
+                self.source_folder = folder
+                self.selected_files = []  # Clear files if folder selected
+                display_path = truncate_path(folder)
+                self.source_path_label.setText(display_path)
+                self.source_path_label.setToolTip(folder)
 
     def select_destination(self):
         folder = QFileDialog.getExistingDirectory(self, "Select Destination Folder")
@@ -148,19 +119,23 @@ class ImageImporter(QWidget):
             self.backup_folder = folder
 
     def start_import(self):
-        if not self.source_folder or not self.dest_folder:
-            self.status_label.setText("Error: Select both source and destination folders.")
+        if not (self.selected_files or self.source_folder) or not self.dest_folder:
+            self.status_label.setText("Error: Select source (folder or files) and destination.")
             return
 
         self.import_button.setEnabled(False)
         self.progress.setValue(0)
         self.status_label.setText("Starting import...")
 
+        metadata = self.metadata_panel.get_metadata() if self.metadata_dropdown.currentText() == "Yes" else {}
+
         self.worker = ImportWorker(
-            self.source_folder,
-            self.dest_folder,
-            self.backup_folder,
-            self.structure_dropdown.currentText()
+            source_folder=self.source_folder,
+            source_files=self.selected_files,
+            dest_folder=self.dest_folder,
+            backup_folder=self.backup_folder,
+            structure=self.structure_dropdown.currentText(),
+            metadata=metadata
         )
         self.thread = QThread()
         self.worker.moveToThread(self.thread)
